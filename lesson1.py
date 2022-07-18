fb83e1c4b5a47cb961c3067bcd7643f15af8a9d9
Nurmuhammad <sharobiddinov0419@gmail.com>
2022-07-18 12:21:11 +0500

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+"""
+Author: Sharobiddinov Nurmuhammad
+
+Sana: 18.07.2022
+
+"""
+
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+# 5 ning 4-darajasini toping.
+print("5 nig 4-darajasi", 5**4,"ga teng")
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4 ga bo'lganda qoldiq", 22 % 4,"ga teng")
+# Tomonlari 125 ga teng kvadrat yuzi va perimetrini toping.
+print("Tomonlari 125 ga teng kvadrat yuzi =", 125**2, "ga perimetri = ", 4 * 125, "ga teng")
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping(pi = 3.14)
+print("Diametri 12 ga teng bo'lgan doira yuzi =", 3.14 * (12/2)**2)
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasining toping.
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi:",(6**2 + 7**2)**(1/2))
