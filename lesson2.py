d5d7bb5995565ad77d01ab8d7aadf0d2e4697316
Nurmuhammad <sharobiddinov0419@gmail.com>
2022-07-18 13:04:06 +0500

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Sharobiddinov Nurmuhammad
+
+Sana: 18.07.2022
+"""
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+matn = "Hello World!"
+print(matn)
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = 'Assalomu alaykum'
+print(xabar)
