7b94ed58f75d3f880d1b938f067fd9d11a65eca6
Nurmuhammad <sharobiddinov0419@gmail.com>
2022-07-18 13:05:04 +0500

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Author: Sharobiddinov Nurmuhammad
 
@@ -7,6 +6,16 @@ Sana: 18.07.2022
 # "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
 matn = "Hello World!"
 print(matn)
+
 # xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
 xabar = 'Assalomu alaykum'
 print(xabar)
+
+# class deb nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# class nomli o'zgaruvchi yaratib bo'lmaydi, chunki u maxsus kalit so'z hisoblanadi
+
+#Quyidagi kodni bajaring
+radius = 5
+pi = 3.14159
+yuza = pi * radius**2
+print("Radiusi", radius, "ga teng doira yuzi =", yuza)
